03d2dd9f38dcff7a61188f199347d74da64c637d
Eun Kyoung Jo <jek@Euns-MacBook-Pro.local>
2018-09-30 12:10:29 +0900

    to use Hangul fonts in matplotlib

---
+++ han_fonts.py
@@ -0,0 +1,44 @@
+#2018/09
+import matplotlib.pyplot as plt
+import matplotlib.font_manager as fm
+import platform
+system_name = platform.system()
+print(system_name)
+
+def set_Han_font():
+    if system_name == 'Darwin':
+        #download https://github.com/ubermenschjo/dotfiles/blob/master/.fonts/HANBatang.ttf to '/Library/Fonts'
+        #rm /Users/jek/.matplotlib/*
+        '''import matplotlib
+        print(matplotlib.get_cachedir())'''
+        #refer to https://github.com/matplotlib/matplotlib/issues/8427/
+        font_name = fm.FontProperties(fname='/Library/Fonts/HANBatang.ttf').get_name()
+    elif system_name == 'Linux':
+        ##apt-get install fonts-unfonts-core fonts-unfonts-extra
+        #maybe restart is needed
+        #rm ~/.cache/matplotlib
+        font_name = fm.FontProperties(fname='/usr/share/fonts/truetype/unfonts-core/UnBatang.ttf').get_name()
+    else: #Windows
+        font_name = fm.FontProperties(fname='C:/Windows/Fonts/HBATANG.ttf').get_name()
+    plt.rc('font', family=font_name)
+    return plt
+
+def draw_bar_chart(words_freqs):
+    words = [w for w, _ in words_freqs]
+    freqs = [f for _, f in words_freqs]
+    xs=[i+0.5 for i, _ in enumerate(words)]
+    plt.bar(xs, freqs)
+    plt.xticks(xs, words)
+    plt.ylabel('# of words')
+    plt.xlabel('top %s occurred words' %len(words_freqs))
+    plt.show()
+    return
+
+
+
+if __name__ == "__main__":
+    try:
+        set_Han_font()
+        draw_bar_chart([('한글',3), ('한국학',5), ('Korea',1)])
+    except Exception as e:
+        print(e)
