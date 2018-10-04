d01f15885277b0188c3a945c91d5b372fea04b78
jo <jek.cl.nlp@daum.net>
2018-10-04 21:35:38 +0900

    utilise external functions with import

--- wc_with_counter_draw.py
+++ wc_with_counter_draw.py
@@ -1,8 +1,8 @@
 #2018/10
 
 from tokenise_ko_with_nltk import *
-#all_tokens = tokenise_with_nltk('ko_input.txt')
-all_tokens = tokenise_with_nltk('한국학2017.09.01-2017.09.07.txt')
+all_tokens = tokenise_with_nltk('ko_input.txt')
+#all_tokens = tokenise_with_nltk('한국학2017.09.01-2017.09.07.txt')
 
 from collections import Counter
 # it gives the sorted result by it's value
