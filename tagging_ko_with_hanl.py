1ee8b7187ef643623d067372679b24fd21d5eebd
jo <jek.cl.nlp@daum.net>
2018-10-16 18:18:29 +0900

    add hangul analyser

---
+++ tagging_ko_with_hanl.py
@@ -0,0 +1,22 @@
+#coding: utf-8
+import os
+os.environ['HANL_HOME'] = '../2018/HANL'
+
+import subprocess
+
+def hanl_analyse(infn, outfn):
+    cmd = '../2018/HANL/bin/hanl < %s > %s' %(infn, outfn)
+    subprocess.call(cmd, shell=True)
+
+    outfile = open(outfn, 'r', encoding='utf-8')
+    lines = outfile.readlines()
+    outfile.close()
+    tokens = []
+    for line in lines:
+        l_token = line.split(' ')
+        tokens.extend(l_token)
+    return tokens
+
+all_tokens = hanl_analyse('ko_input.txt', 'ko_input.txt.out')
+
+
