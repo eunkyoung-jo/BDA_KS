4ab31bc503d896801e3bb959578f1fd89211c81e
Eun Kyoung Jo <jek@Euns-MacBook-Pro.local>
2018-10-18 16:51:49 +0900

    how to use shell command

--- tagging_ko_with_hanl.py
+++ tagging_ko_with_hanl.py
@@ -7,16 +7,8 @@ import subprocess
 def hanl_analyse(infn, outfn):
     cmd = '../2018/HANL/bin/hanl < %s > %s' %(infn, outfn)
     subprocess.call(cmd, shell=True)
+    return
 
-    outfile = open(outfn, 'r', encoding='utf-8')
-    lines = outfile.readlines()
-    outfile.close()
-    tokens = []
-    for line in lines:
-        l_token = line.split(' ')
-        tokens.extend(l_token)
-    return tokens
-
-all_tokens = hanl_analyse('ko_input.txt', 'ko_input.txt.out')
+hanl_analyse('ko_input.txt', 'ko_input.txt.out')
 
 
