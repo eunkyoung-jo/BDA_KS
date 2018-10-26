b24041d068fea7681ab6f61e7b2296951bdb5fa6
jo <jek.cl.nlp@daum.net>
2018-10-27 03:32:09 +0900

    interacting with web API, json. toxlsx

--- json2xlsx.py
+++ json2xlsx.py
@@ -1,15 +1,31 @@
 # coding: utf-8
 #2018/10/28
+# refer to p.186~188 in Python for Data analysis
 
 import requests
-url='https://api.github.com/repos/pandas-dev/pandas/issues'
-resp = requests.get(url)
-data = resp.json()
-data[0]['title']
 import pandas as pd
-issues = pd.DataFrame(data)
-issues
-issues = pd.DataFrame(data, columns=['url', 'html_url', 'title', 'id', 'user'])
-writer = pd.ExcelWriter('data/issues.xlsx')
-issues.to_excel(writer, 'fromGithub')
-writer.save()
+
+
+url='https://api.github.com/repos/pandas-dev/pandas/issues'
+def json2xlsx(url, sheetname='sheet1', filename='out.xlsx'):
+
+    resp = requests.get(url)
+    data = resp.json()
+    data[0]['title']
+    issues = pd.DataFrame(data)
+    issues
+    issues = pd.DataFrame(data, columns=['url', 'html_url', 'title', 'id', 'user'])
+    writer = pd.ExcelWriter(filename)
+    issues.to_excel(writer, sheetname)
+    writer.save()
+
+def xlsx2txt(xlsfn, outfn='out.txt'):
+    data = pd.read_excel(xlsfn, index_col=None, headers=None)
+    #data = pd.read_excel(xlsfn)
+    print(data)
+    data.to_csv(outfn, sep='\t')
+
+if __name__ == "__main__":
+    #json2xlsx(url)
+    #xlsx2txt('out.xlsx')
+    json2xlsx(url, 'fromGithub', 'data/issues.xlsx')
