b2b624c1603740929f92834d86d17738a08c8a08
jo <jek.cl.nlp@daum.net>
2018-11-27 08:46:04 +0900

    handle json, json3xlsx

--- util_json.py
+++ util_json.py
@@ -4,18 +4,7 @@ import requests, json
 import pandas as pd
 
 def show_json_data(key='', url=None):
-    if url is None:
-        json_str='{"one" : "하나", "two" : "둘", "three" : "셋"}'
-        json_data = json.loads(json_str)
-        print(json_data)
-        print(json_data.get(key, 'None'))
-        return
-    #if url is given
-    wdata = requests.get(url)
-    jdata = json.loads(wdata.text) # == wdata.json()
-    for jele in jdata:
-        print(jele.get('url', 'None'))
-        print('[title]', jele.get('title', 'None'))
+    #fill out codes
 
 def json2xlsx(url, sheetname='sheet1', filename='out.xlsx'):
     resp = requests.get(url)
