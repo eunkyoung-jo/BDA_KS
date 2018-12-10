912e5de94f5478890406843009cfe2107fe72f08
jo <jek.cl.nlp@daum.net>
2018-12-10 22:01:32 +0900

    stopwords

---
+++ stopwords_eng_kor.py
@@ -0,0 +1,12 @@
+#2018/12
+
+from nltk.corpus import stopwords
+print(stopwords.words('english'))
+print(len(stopwords.words('english')))
+
+print(stopwords.words('korean'))
+print(len(stopwords.words('korean')))
+
+#cat ~/nltk_data/corpora/stopwords/korean
+#이, 가, 는, 을, 를, 에게, 나, 너, 우리, 저희, 그, 그녀, 이다, 그리고, 었, 겠, ㄹ
+
