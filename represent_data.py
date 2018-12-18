d270bf72b4cfa70c689e15a7078dcdd3a23dc748
jo <jek.cl.nlp@daum.net>
2018-12-18 13:57:53 +0900

    represent data with numpy and matplotlib

--- represent_data.py
+++ represent_data.py
@@ -7,11 +7,10 @@ def prepare_data(num_points, a, b):
     for i in range(num_points):
         x = np.random.normal(0.0, 0.5)
         y = x * a + np.random.normal(0.0, 0.03) * b
-        d_vector.append([x, y])
+        d_vector.append([x,y])
     print(d_vector)
-    # fill here
-
-
+    x_data = [d[0] for d in d_vector]
+    y_data = [d[1] for d in d_vector]
     return x_data, y_data
 
 if __name__ == "__main__":
